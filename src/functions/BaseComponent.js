1a02f9c098f3368792b40ac1b9aab264f967cd71
Ahmed Rafik Ibrahim <ahmedrafik_maaar@outlook.com>
2020-10-19 12:28:04 +0200

    Add some descriptive comments

--- src/functions/BaseComponent.js
+++ src/functions/BaseComponent.js
@@ -9,8 +9,8 @@ function BaseComponent(el, componentName) {
   el.style.display = 'unset';
   el.style.position = 'unset';
 
-  // get props
-  this.parseValue = (strValue) => {
+  // get props from attributes
+  this.parseAttribute = (strValue) => {
     try {
       return JSON.parse(strValue);
     } catch (e) {
@@ -18,25 +18,29 @@ function BaseComponent(el, componentName) {
     }
   }
 
+  // Define props
   Object.values(el.attributes).forEach((attribute) => {
     if (attribute.name[0] === ':') {
-      this[attribute.name.substring(1)] = this.parseValue(attribute.value);
+      this[attribute.name.substring(1)] = this.parseAttribute(attribute.value);
     }
   })
 
+  // get template
   this.template = document.querySelector(`template#${componentName}`).innerHTML;
 
+  // Mompute the final value of a path or properties from this object
   this.getPathValue = (baseObject, pathString, stripQuotes = false) => {
 
+    // Used to get a function's return value
     const callFunctionFromPath = (baseObject, functionPath) => {
       const [functionName, paramsStr] = functionPath.slice(0, -1).split('(')
       const paramsArr = paramsStr.replaceAll(/(\s|\t|\n|\r)/g, '').split(',');
       return baseObject[functionName](...paramsArr);
     }
 
+    // Create a string containing an Immediately-invoked Function to use for event attributes 'e.g. onclick'
     // is an Immediately-invoked Function
     let isIIFE = false;
-
     const stringfyFunction = (func) => {
       isIIFE = true;
       let strFunc = `(${func.toString()})()`;
@@ -47,6 +51,7 @@ function BaseComponent(el, componentName) {
       return strFunc;
     }
 
+    // loop though path
     const value = pathString.split('.').reduce(
       (target, current) => current.endsWith(')')
         ? callFunctionFromPath(target, current)
@@ -55,15 +60,19 @@ function BaseComponent(el, componentName) {
           : target[current],
     baseObject);
 
+    // quates should always be used around strings unless this is an attribute inside DOM
     return stripQuotes || isIIFE ? value : `'${value}'`;
   }
 
+  // Manipulate template string to replace placeholders with actual data
   this.evaluateTemplate = () => {
     let evaluation = this.template;
+    // evaluate placeholders
     const curlyMatches = [...evaluation.matchAll(/{{(.*)}}/g)]
     curlyMatches.forEach(([expression , path]) => {
       evaluation = evaluation.replace(expression, this.getPathValue(this, path, true))
     })
+    // evaluate attribute binding
     const columnMatches = [...evaluation.matchAll(/:(\w+)="(.*)"/g)]
     columnMatches.forEach(([expression , attribute, path]) => {
       evaluation = evaluation.replace(expression, `${attribute}="${this.getPathValue(this, path)}"`)
