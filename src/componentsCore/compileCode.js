5aa3ad4bc347b62ec32ed87baf673c72ff359b46
Ahmed Rafik Ibrahim <ahmedrafik_maaar@outlook.com>
2020-10-20 14:46:51 +0200

    Create a safer compile function to use

---
+++ src/componentsCore/compileCode.js
@@ -0,0 +1,21 @@
+const sandboxProxies = new WeakMap()
+
+function compileCode (codeToCompile, sandbox) {
+  const src = `with (sandbox) { return ${codeToCompile}}`
+  const code = new Function('sandbox', src)
+
+  if (!sandboxProxies.has(sandbox)) {
+    const sandboxProxy = new Proxy(sandbox, {has, get})
+    sandboxProxies.set(sandbox, sandboxProxy)
+  }
+  return code(sandboxProxies.get(sandbox))
+}
+
+function has (target, key) {
+  return true
+}
+
+function get (target, key) {
+  if (key === Symbol.unscopables) return undefined
+  return target[key]
+}
