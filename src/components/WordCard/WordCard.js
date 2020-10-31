28c59a6841bb4d9abaf4be1f272691f6db4eaedd
Ahmed Rafik Radwan <ahmedrafik_maaar@outlook.com>
2020-10-31 13:56:48 +0200

    refactor word card

--- src/components/WordCard/WordCard.js
+++ src/components/WordCard/WordCard.js
@@ -1,9 +1,9 @@
 function WordCard(el) {
-  BaseComponent.call(this, el, 'WordCard')
+  BaseComponent.call(this, el, 'WordCard');
 
   // define state
-  this.speak = (event, word) => {
-    textToSpeach(word || this.props.entry.word)
+  this.speak = (word) => {
+    textToSpeach(word || this.props.entry.word);
   }
 
   this.render();
